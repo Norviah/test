3b93c4c75cac6f7226202122b9a16598e4506d84
Norviah <21983700+Norviah@users.noreply.github.com>
2024-10-18 02:17:58 -0700

    idk

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,8 +1,8 @@
 import { electronApp, optimizer } from '@electron-toolkit/utils';
-import { BrowserWindow, app, ipcMain } from 'electron';
+import { autoUpdater } from 'electron-updater';
+import { BrowserWindow, Menu, app, ipcMain } from 'electron';
 
 import { registerAPI } from './systems/ipc';
-import { initializeTray } from './systems/tray';
 
 import * as windows from './systems/window';
 
@@ -10,6 +10,8 @@ import * as windows from './systems/window';
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  autoUpdater.checkForUpdatesAndNotify();
+
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron');
 
@@ -20,7 +22,7 @@ app.whenReady().then(() => {
     optimizer.watchWindowShortcuts(window);
   });
 
-  initializeTray();
+  // initializeTray();
   registerAPI(ipcMain);
 
   windows.main.open();
@@ -41,4 +43,82 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
+});
+
+import { Logger } from '@norviah/logger';
+
+const logger = new Logger({ write: true, dir: app.getPath('desktop') });
+
+const template: any = [];
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about',
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  });
+}
+
+// let win: BrowserWindow | null = null;
+
+function sendStatusToWindow(text) {
+  logger.info(text);
+  windows.main.window?.webContents.send('message', text);
+}
+// function createDefaultWindow() {
+//   win = new BrowserWindow({
+//     webPreferences: {
+//       nodeIntegration: true,
+//       contextIsolation: false,
+//     },
+//   });
+//   win.webContents.openDevTools();
+//   win.on('closed', () => {
+//     win = null;
+//   });
+//   win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+//   return win;
+// }
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', () => {
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', () => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on('update-downloaded', () => {
+  sendStatusToWindow('Update downloaded');
+});
+app.on('ready', () => {
+  // Create the Menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  // createDefaultWindow();
+});
+app.on('window-all-closed', () => {
+  app.quit();
 });
